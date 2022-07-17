e75173573e6a4fe12b09733032351fb501139967
emrek <emre.kahraman@outlook.com>
2022-07-17 16:40:12 +0300

    a

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ mesafe = 100
 a = random.randint(30,170)
 
 while deneme > 0:
+    print("Nişangah yeri: ", a)
     aci = int(input("Açı giriniz:"))
     hiz = int(input("Hız giriniz:"))
     y = yukseklik(aci, hiz, mesafe)
@@ -16,26 +17,22 @@ while deneme > 0:
         deneme = deneme - 1
         print("hakkınız:",deneme)
         print("İsabet noktası: ",abs(a - y))
-        print("Nişangah yeri: ",a)
         print("Puanınız:",puan)
     elif (10 <(abs(a-y))<= 20):
         puan = puan + 3
         deneme = deneme - 1
         print("hakkınız:", deneme)
         print("İsabet noktası: ", abs(a - y))
-        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
     elif (20 <(abs(a-y))<= 30):
         puan = puan + 2
         deneme = deneme - 1
         print("hakkınız:", deneme)
         print("İsabet noktası: ", abs(a - y))
-        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
     elif (30 <(abs(a-y))):
         puan = puan + 1
         deneme = deneme - 1
         print("hakkınız:", deneme)
         print("İsabet noktası: ", abs(a - y))
-        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
