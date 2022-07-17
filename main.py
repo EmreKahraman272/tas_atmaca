de6be3bcf80860278b4acd0e0d35cfc06ad5eca6
emrek <emre.kahraman@outlook.com>
2022-07-17 16:38:30 +0300

    a

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 import math
 import random
-
+from yukseklik import yukseklik
 
 puan = 0
 deneme = 3
+mesafe = 100
+a = random.randint(30,170)
 
 while deneme > 0:
-    a = random.randint(1, 200)
-    b = 120
-    if (0 <(abs(a-b))<= 10):
+    aci = int(input("Açı giriniz:"))
+    hiz = int(input("Hız giriniz:"))
+    y = yukseklik(aci, hiz, mesafe)
+    if (0 <(abs(a-y))<= 10):
         puan = puan + 4
         deneme = deneme - 1
         print("hakkınız:",deneme)
-        print(abs(a - b))
-        print(a)
+        print("İsabet noktası: ",abs(a - y))
+        print("Nişangah yeri: ",a)
         print("Puanınız:",puan)
-    if (10 <(abs(a-b))<= 20):
+    elif (10 <(abs(a-y))<= 20):
         puan = puan + 3
         deneme = deneme - 1
         print("hakkınız:", deneme)
-        print(abs(a - b))
-        print(a)
+        print("İsabet noktası: ", abs(a - y))
+        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
-    if (20 <(abs(a-b))<= 30):
+    elif (20 <(abs(a-y))<= 30):
         puan = puan + 2
         deneme = deneme - 1
         print("hakkınız:", deneme)
-        print(abs(a - b))
-        print(a)
+        print("İsabet noktası: ", abs(a - y))
+        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
-    if (30 <(abs(a-b))):
+    elif (30 <(abs(a-y))):
         puan = puan + 1
         deneme = deneme - 1
         print("hakkınız:", deneme)
-        print(abs(a-b))
-        print(a)
+        print("İsabet noktası: ", abs(a - y))
+        print("Nişangah yeri: ", a)
         print("Puanınız:", puan)
